14b7e0619fafd65e1a3260d2d17e559f97b536e9
Alex Mubarakshin <muban96@mail.ru>
2019-11-05 14:13:16 +0300

    ✨ Add animations support

--- src/complnents/Dancer/index.tsx
+++ src/complnents/Dancer/index.tsx
@@ -2,20 +2,24 @@ import * as React from 'react';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Vector3, AnimationMixer } from 'three';
-
-const SHREK_MODEL_URL = '/models/adrilik_shrek/scene.gltf';
+import { useFrame } from 'react-three-fiber';
 
 type ShrekProps = {
   position?: Vector3 | number[];
+  src: string;
+  scale?: Vector3 | number[];
 }
 
-const Shrek: React.FC<ShrekProps> = ({ position }) => {
+const Shrek: React.FC<ShrekProps> = ({ position, src, scale }) => {
   const groupRef = React.useRef();
+
+  const speed = 1;
 
   const [mixer] = React.useState(() => new AnimationMixer(undefined));
 
-  const [gltf, setGLTF] = React.useState()
-  React.useMemo(() => new GLTFLoader().load(SHREK_MODEL_URL, setGLTF), [SHREK_MODEL_URL]);
+  const [gltf, setGLTF] = React.useState();
+
+  React.useMemo(() => new GLTFLoader().load(src, setGLTF), [src]);
 
   React.useEffect(() => {
     if (gltf) {
@@ -23,16 +27,19 @@ const Shrek: React.FC<ShrekProps> = ({ position }) => {
       action.play();
     }
   }, [gltf, mixer]);
-  
+
+  useFrame((_state, delta) => {
+    if (gltf) {
+      mixer.update(delta * speed);
+    }
+  });
+
   if (!gltf) {
     return null;
   }
-  
-  // debugger;
 
   return (
-
-    <group ref={groupRef}>
+    <group ref={groupRef} scale={scale}>
       <primitive object={gltf.scene} position={position} />
     </group>
   );
--- src/containers/home/HomeContainer.tsx
+++ src/containers/home/HomeContainer.tsx
@@ -12,7 +12,10 @@ import './HomeContainer.styles.scss';
 
 import Plane from '../../complnents/Plane';
 import Controls from '../../complnents/Controls';
-import Shrek from '../../complnents/Shrek';
+import Dancer from '../../complnents/Dancer';
+
+const SHREK_MODEL_URL = '/models/adrilik_shrek/scene.gltf';
+const STORM_TROOPER_MODEL_URL = '/models/stormtrooper/scene.gltf';
 
 extend({ OrbitControls });
 
@@ -20,7 +23,7 @@ export default () => (
   <>
     <Canvas
       camera={{
-        position: [0, 2, 1]
+        position: [0, 3, 1]
       }}
       onCreated={({ gl }) => {
         gl.shadowMap.enabled = true;
@@ -28,7 +31,7 @@ export default () => (
       }}>
       {
         // @ts-ignore
-        <fog attach="fog" args={["#fff", 10, 20]} />
+        <fog attach="fog" args={["#fff4e1", 10, 20]} />
       }
 
       <ambientLight />
@@ -39,8 +42,10 @@ export default () => (
         maxPolarAngle={Math.PI / 1.8}
       />
 
-      <Shrek position={[0, -1, 0]} />
-      <Plane position={[0, -1, 0]} color="white" />
+      <Dancer position={[0, -1, 0]} src={SHREK_MODEL_URL} />
+      <Dancer position={[-2, -1, -3]} src={STORM_TROOPER_MODEL_URL} />
+
+      <Plane position={[0, -1, 0]} color="#9ddcdc" />
     </Canvas>
   </>
 );
