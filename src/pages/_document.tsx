e2e4e6cc4f7ea01ac892517f8d37686ada17270e
Alex Mubarakshin <muban96@mail.ru>
2019-11-08 17:29:39 +0300

    📝 Readme update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
 export default class extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
       <Html lang="en">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NextPage } from 'next';
 
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 const HomeContainer = dynamic(
   () => import('../containers/home/HomeContainer'),
@@ -9,7 +10,15 @@ const HomeContainer = dynamic(
 );
 
 const IndexPage: NextPage = () => (
-  <HomeContainer />
+  <>
+    <Head>
+      <title>🐸 Personal experiments with react-three-fiber</title>
+      <meta name="description" content="Personal experiments with react-three-fiber"></meta>
+      <meta name="og:title" property="og:title" content="Personal experiments with react-three-fiber"></meta>
+      <meta name="robots" content="index, follow"></meta>
+    </Head>
+    <HomeContainer />
+  </>
 )
 
 export default IndexPage;
